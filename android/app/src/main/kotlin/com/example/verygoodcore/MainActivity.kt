ab6d076dbfc0bb2a5271b72ae447c2f9cd382ada
Klaudiusz <klaudiusz.kalinowski01@wp.pl>
2024-02-08 22:10:28 +0100

    initial app

---
+++ android/app/src/main/kotlin/com/example/verygoodcore/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.verygoodcore.second_hand_app
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main_development.dart
@@ -0,0 +1,6 @@
+import 'package:second_hand_app/app/app.dart';
+import 'package:second_hand_app/bootstrap.dart';
+
+void main() {
+  bootstrap(() => const App());
+}
