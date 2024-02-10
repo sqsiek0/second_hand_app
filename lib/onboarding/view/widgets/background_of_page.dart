03ffe6d53aad6355812e28b1686359ccf4021eb3
Klaudiusz <klaudiusz.kalinowski01@wp.pl>
2024-02-10 21:53:40 +0100

    save

---
+++ lib/onboarding/view/widgets/background_of_page.dart
@@ -0,0 +1,39 @@
+import 'package:flutter/material.dart';
+
+class BackgroundPage extends StatelessWidget {
+  const BackgroundPage({required this.child, super.key});
+  final Widget child;
+
+  @override
+  Widget build(BuildContext context) {
+    return Stack(
+      children: [
+        Positioned(
+          top: -MediaQuery.of(context).size.height * 0.1,
+          left: -MediaQuery.of(context).size.width * 0.6,
+          child: Container(
+            width: MediaQuery.of(context).size.width * 1.5,
+            height: MediaQuery.of(context).size.width * 1.5,
+            decoration: BoxDecoration(
+              borderRadius: BorderRadius.circular(1000),
+              color: Colors.red.shade100,
+            ),
+          ),
+        ),
+        Positioned(
+          top: MediaQuery.of(context).size.height * 0.2,
+          left: MediaQuery.of(context).size.width * 0.25,
+          child: Container(
+            width: MediaQuery.of(context).size.width * 1.5,
+            height: MediaQuery.of(context).size.width * 1.5,
+            decoration: BoxDecoration(
+              borderRadius: BorderRadius.circular(1000),
+              color: Colors.red.shade200,
+            ),
+          ),
+        ),
+        child,
+      ],
+    );
+  }
+}
--- lib/onboarding/view/widgets/login_page.dart
+++ lib/onboarding/view/widgets/login_page.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
 import 'package:second_hand_app/app/view/widgets/app_textfield.dart';
+import 'package:second_hand_app/onboarding/view/widgets/background_of_page.dart';
 
 class LoginPage extends StatefulWidget {
   const LoginPage({super.key});
@@ -30,189 +31,163 @@ class _LoginPageState extends State<LoginPage> {
   @override
   Widget build(BuildContext context) {
     return Material(
-      child: Stack(
-        children: [
-          Positioned(
-            top: -MediaQuery.of(context).size.height * 0.1,
-            left: -MediaQuery.of(context).size.width * 0.6,
-            child: Container(
-              width: MediaQuery.of(context).size.width * 1.5,
-              height: MediaQuery.of(context).size.width * 1.5,
-              decoration: BoxDecoration(
-                borderRadius: BorderRadius.circular(1000),
-                color: Colors.red.shade100,
-              ),
+      child: BackgroundPage(
+        child: SafeArea(
+          child: Padding(
+            padding: EdgeInsets.only(
+              left: 16,
+              right: 16,
+              bottom: MediaQuery.of(context).viewInsets.bottom,
             ),
-          ),
-          Positioned(
-            top: MediaQuery.of(context).size.height * 0.2,
-            left: MediaQuery.of(context).size.width * 0.25,
-            child: Container(
-              width: MediaQuery.of(context).size.width * 1.5,
-              height: MediaQuery.of(context).size.width * 1.5,
-              decoration: BoxDecoration(
-                borderRadius: BorderRadius.circular(1000),
-                color: Colors.red.shade200,
-              ),
-            ),
-          ),
-          SafeArea(
-            child: Padding(
-              padding: EdgeInsets.only(
-                left: 16,
-                right: 16,
-                bottom: MediaQuery.of(context).viewInsets.bottom,
-              ),
-              child: Center(
-                child: SingleChildScrollView(
-                  child: Column(
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: [
-                      Text(
-                        'Witaj ponownie!',
-                        textAlign: TextAlign.center,
-                        style: GoogleFonts.syne(
-                          fontSize: 32,
-                          fontWeight: FontWeight.w700,
-                        ),
+            child: Center(
+              child: SingleChildScrollView(
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Text(
+                      'Witaj ponownie!',
+                      textAlign: TextAlign.center,
+                      style: GoogleFonts.syne(
+                        fontSize: 32,
+                        fontWeight: FontWeight.w700,
                       ),
-                      Text(
-                        'Zaloguj się na swoje konto i szukaj nowych perełek!',
-                        textAlign: TextAlign.left,
-                        style: GoogleFonts.syne(
-                          fontSize: 16,
-                          color: Colors.grey.shade800,
-                          fontWeight: FontWeight.w400,
-                        ),
+                    ),
+                    Text(
+                      'Zaloguj się na swoje konto i szukaj nowych perełek!',
+                      textAlign: TextAlign.left,
+                      style: GoogleFonts.syne(
+                        fontSize: 16,
+                        color: Colors.grey.shade800,
+                        fontWeight: FontWeight.w400,
                       ),
-                      const SizedBox(height: 60),
-                      Column(
-                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                        crossAxisAlignment: CrossAxisAlignment.end,
-                        children: [
-                          Form(
-                            child: Column(
-                              children: [
-                                AppTextField(
-                                  hintText: 'E-mail',
-                                  icon: const Icon(Icons.email_outlined),
-                                  controller: emailController,
-                                ),
-                                const SizedBox(height: 20),
-                                AppTextField(
-                                  hintText: 'Hasło',
-                                  icon: const Icon(Icons.lock_outlined),
-                                  isPassword: true,
-                                  controller: passwordController,
-                                ),
-                              ],
-                            ),
-                          ),
-                          const SizedBox(
-                            height: 5,
-                          ),
-                          InkWell(
-                            onTap: () {},
-                            child: Text(
-                              'Nie pamietasz hasła?',
-                              textAlign: TextAlign.left,
-                              style: GoogleFonts.syne(
-                                fontSize: 14,
-                                color: Colors.black,
-                                fontWeight: FontWeight.w500,
-                              ),
-                            ),
-                          ),
-                          const SizedBox(height: 30),
-                          InkWell(
-                            onTap: () {},
-                            child: Container(
-                              width: double.infinity,
-                              decoration: BoxDecoration(
-                                borderRadius: BorderRadius.circular(100),
-                                color: Colors.black,
-                              ),
-                              child: Padding(
-                                padding: const EdgeInsets.symmetric(
-                                  vertical: 20,
-                                ),
-                                child: Text(
-                                  'Zaloguj się',
-                                  textAlign: TextAlign.center,
-                                  style: GoogleFonts.syne(
-                                    color: Colors.white,
-                                    fontSize: 16,
-                                    fontWeight: FontWeight.w600,
-                                  ),
-                                ),
-                              ),
-                            ),
-                          ),
-                          const SizedBox(height: 10),
-                          Row(
-                            mainAxisAlignment: MainAxisAlignment.center,
+                    ),
+                    const SizedBox(height: 60),
+                    Column(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      crossAxisAlignment: CrossAxisAlignment.end,
+                      children: [
+                        Form(
+                          child: Column(
                             children: [
-                              Expanded(
-                                child: Container(
-                                  height: 1,
-                                  color: Colors.grey.shade700,
-                                ),
+                              AppTextField(
+                                hintText: 'E-mail',
+                                icon: const Icon(Icons.email_outlined),
+                                controller: emailController,
                               ),
-                              Padding(
-                                padding:
-                                    const EdgeInsets.symmetric(horizontal: 10),
-                                child: Text(
-                                  'lub',
-                                  style: GoogleFonts.syne(
-                                    fontWeight: FontWeight.w500,
-                                    fontSize: 14,
-                                    color: Colors.black,
-                                  ),
-                                ),
-                              ),
-                              Expanded(
-                                child: Container(
-                                  height: 1,
-                                  color: Colors.grey.shade700,
-                                ),
+                              const SizedBox(height: 20),
+                              AppTextField(
+                                hintText: 'Hasło',
+                                icon: const Icon(Icons.lock_outlined),
+                                isPassword: true,
+                                controller: passwordController,
                               ),
                             ],
                           ),
-                          const SizedBox(height: 10),
-                          InkWell(
-                            onTap: () {},
-                            child: Container(
-                              width: double.infinity,
-                              decoration: BoxDecoration(
-                                borderRadius: BorderRadius.circular(100),
-                                color: Colors.grey.shade200,
-                                border: Border.all(),
+                        ),
+                        const SizedBox(
+                          height: 5,
+                        ),
+                        InkWell(
+                          onTap: () {},
+                          child: Text(
+                            'Nie pamietasz hasła?',
+                            textAlign: TextAlign.left,
+                            style: GoogleFonts.syne(
+                              fontSize: 14,
+                              color: Colors.black,
+                              fontWeight: FontWeight.w500,
+                            ),
+                          ),
+                        ),
+                        const SizedBox(height: 30),
+                        InkWell(
+                          onTap: () {},
+                          child: Container(
+                            width: double.infinity,
+                            decoration: BoxDecoration(
+                              borderRadius: BorderRadius.circular(100),
+                              color: Colors.black,
+                            ),
+                            child: Padding(
+                              padding: const EdgeInsets.symmetric(
+                                vertical: 20,
                               ),
-                              child: Padding(
-                                padding: const EdgeInsets.symmetric(
-                                  vertical: 20,
-                                ),
-                                child: Text(
-                                  'Zarejestruj się',
-                                  textAlign: TextAlign.center,
-                                  style: GoogleFonts.syne(
-                                    color: Colors.black,
-                                    fontSize: 16,
-                                    fontWeight: FontWeight.w600,
-                                  ),
+                              child: Text(
+                                'Zaloguj się',
+                                textAlign: TextAlign.center,
+                                style: GoogleFonts.syne(
+                                  color: Colors.white,
+                                  fontSize: 16,
+                                  fontWeight: FontWeight.w600,
                                 ),
                               ),
                             ),
                           ),
-                        ],
-                      ),
-                    ],
-                  ),
+                        ),
+                        const SizedBox(height: 10),
+                        Row(
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          children: [
+                            Expanded(
+                              child: Container(
+                                height: 1,
+                                color: Colors.grey.shade700,
+                              ),
+                            ),
+                            Padding(
+                              padding:
+                                  const EdgeInsets.symmetric(horizontal: 10),
+                              child: Text(
+                                'lub',
+                                style: GoogleFonts.syne(
+                                  fontWeight: FontWeight.w500,
+                                  fontSize: 14,
+                                  color: Colors.black,
+                                ),
+                              ),
+                            ),
+                            Expanded(
+                              child: Container(
+                                height: 1,
+                                color: Colors.grey.shade700,
+                              ),
+                            ),
+                          ],
+                        ),
+                        const SizedBox(height: 10),
+                        InkWell(
+                          onTap: () {},
+                          child: Container(
+                            width: double.infinity,
+                            decoration: BoxDecoration(
+                              borderRadius: BorderRadius.circular(100),
+                              color: Colors.grey.shade200,
+                              border: Border.all(),
+                            ),
+                            child: Padding(
+                              padding: const EdgeInsets.symmetric(
+                                vertical: 20,
+                              ),
+                              child: Text(
+                                'Zarejestruj się',
+                                textAlign: TextAlign.center,
+                                style: GoogleFonts.syne(
+                                  color: Colors.black,
+                                  fontSize: 16,
+                                  fontWeight: FontWeight.w600,
+                                ),
+                              ),
+                            ),
+                          ),
+                        ),
+                      ],
+                    ),
+                  ],
                 ),
               ),
             ),
           ),
-        ],
+        ),
       ),
     );
   }
